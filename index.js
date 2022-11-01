72bbfa3a668f855a3b49c02afaa97171707c47f1
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-11-01 12:11:25 -0400

    commit tree 30f5a28c5f8d5b78e767f1c502c4f3e90fa41506

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+let cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+];
+
+function destructivelyAppendCat(catName) {
+    cats.push(catName);
+};
+
+function destructivelyPrependCat(catName) {
+    cats.unshift(catName);
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+
+function appendCat(catName) {
+    let newCats = [...cats, catName];
+    return newCats;
+};
+
+function prependCat(catName) {
+    let newCats = [catName, ...cats];
+    return newCats;
+};
+
+function removeLastCat() {
+    let newCats = cats.slice();
+    newCats.pop();  
+    return newCats;
+};
+
+function removeFirstCat() {
+    let newCats = cats.slice();
+    newCats.shift();
+    return newCats;
+};
